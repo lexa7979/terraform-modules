9e1e1110066beba614d8b75e12c48c3e999a8d49
Alexander Urban <lexa@toxa.de>
2022-12-26 17:23:30 +0100

    Testing JSON-configuration

--- variables.tf
+++ variables.tf
@@ -1,29 +1,29 @@
-variable "instance_name" {
-  description = "Value of the Name tag for the EC2 instance"
-  type        = string
-  default     = "ExampleAppServerInstanceJuly22"
-}
+# variable "instance_name" {
+#   description = "Value of the Name tag for the EC2 instance"
+#   type        = string
+#   default     = "ExampleAppServerInstanceJuly22"
+# }
 
-variable "instance_type" {
-  description = "Type of EC2 instance to provision"
-  type        = string
-  default     = "t3.micro"
-}
+# variable "instance_type" {
+#   description = "Type of EC2 instance to provision"
+#   type        = string
+#   default     = "t3.micro"
+# }
 
-variable "instance_ami" {
-  description = "ID of the Amazon Machine Image to be used"
-  type        = string
-  default     = "ami-0440e5026412ff23f"
-}
+# variable "instance_ami" {
+#   description = "ID of the Amazon Machine Image to be used"
+#   type        = string
+#   default     = "ami-0440e5026412ff23f"
+# }
 
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "eu-north-1"
-}
+# variable "region" {
+#   description = "AWS region"
+#   type        = string
+#   default     = "eu-north-1"
+# }
 
-variable "webserver_port" {
-  description = "The port the server will use for HTTP requests"
-  type        = number
-  default     = 8080
-}
+# variable "webserver_port" {
+#   description = "The port the server will use for HTTP requests"
+#   type        = number
+#   default     = 8080
+# }
